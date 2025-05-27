177697d9618d16994f5d555a2645c1ea19ebfa00
Max Alves <maxlek79@gmail.com>
2025-05-27 16:09:40 -0300

    Update WhatsAppSendMessage.cs

--- WhatsAppProject/WhatsAppSendMessage.cs
+++ WhatsAppProject/WhatsAppSendMessage.cs
@@ -9,76 +9,67 @@ using System.Threading.Tasks;
 
 namespace WhatsAppProject
 {
+    // Classe responsável por enviar mensagens no WhatsApp Web
     public class WhatsAppSendMessage : Web
     {
+        // Envia uma mensagem de texto para um contato específico
         public void SendMessage(string message, string to)
         {
-            StartBrowser(TypeDriver.GoogleChorme, "C:\\Users\\Felipe Ribeiro\\AppData\\Local\\Google\\Chrome\\User Data");
+            // Inicia o navegador com o perfil do usuário (mantém login no WhatsApp)
+            StartBrowser(TypeDriver.GoogleChorme, "C:\\Users\\Max alves\\AppData\\Local\\Google\\Chrome\\User Data");
 
+            // Acessa o WhatsApp Web
             Navigate("https://web.whatsapp.com/");
-
             WaitForLoad();
+            Thread.Sleep(TimeSpan.FromSeconds(4)); // Aguarda a interface carregar completamente
 
-            Thread.Sleep(TimeSpan.FromSeconds(4));
-
-            //WaitForElement(TypeElement.Xpath, "//*[@id='side']/div[1]/div/div/div[2]/div/div[2]");
-
+            // Pesquisa o contato e abre a conversa
             var elemenSearch = AssignValue(TypeElement.Xpath, "//*[@id='side']/div[1]/div/div/div[2]/div/div[2]", to, 5);
-
             elemenSearch.element.SendKeys(OpenQA.Selenium.Keys.Enter);
 
+            // Localiza o campo de mensagem e envia o texto
             var elementMessage = AssignValue(TypeElement.Xpath, "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p", message);
-
             elementMessage.element.SendKeys(OpenQA.Selenium.Keys.Enter);
 
-            CloseBrowser();
+            CloseBrowser(); // Fecha o navegador após o envio
         }
 
+        // Envia uma mensagem com imagem anexada
         public void SendMessageWithImage(string messsage, string pathImage, string to)
         {
-            StartBrowser(TypeDriver.GoogleChorme, "C:\\Users\\Felipe Ribeiro\\AppData\\Local\\Google\\Chrome\\User Data");
-
+            StartBrowser(TypeDriver.GoogleChorme, "C:\\Users\\Max alves\\AppData\\Local\\Google\\Chrome\\User Data");
             Navigate("https://web.whatsapp.com/");
-
             WaitForLoad();
-
             Thread.Sleep(TimeSpan.FromSeconds(4));
 
-            //WaitForElement(TypeElement.Xpath, "//*[@id='side']/div[1]/div/div/div[2]/div/div[2]");
-
             var elemenSearch = AssignValue(TypeElement.Xpath, "//*[@id='side']/div[1]/div/div/div[2]/div/div[2]", to, 5);
-
             elemenSearch.element.SendKeys(OpenQA.Selenium.Keys.Enter);
 
-            //Clicar no botão CLIP
+            // Clica no botão de anexar arquivo (clipe)
             Click(TypeElement.Xpath, "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div/span");
 
-            //Adicionar o caminho da Imagem
+            // Anexa a imagem a partir do caminho fornecido
             AssignValue(TypeElement.Xpath, "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button/input", pathImage);
 
+            // Envia a legenda da imagem (opcional)
             var elementInput = AssignValue(TypeElement.Xpath, "//*[@id=\"app\"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[1]/p", messsage);
-
             elementInput.element.SendKeys(OpenQA.Selenium.Keys.Enter);
 
             CloseBrowser();
         }
 
+        // Envia uma mensagem com emojis inseridos por texto
         public void SendMessageWithEmoji(string message, List<string> emojis, string to)
         {
-            StartBrowser(TypeDriver.GoogleChorme, "C:\\Users\\Felipe Ribeiro\\AppData\\Local\\Google\\Chrome\\User Data");
-
+            StartBrowser(TypeDriver.GoogleChorme, "C:\\Users\\Max alves\\AppData\\Local\\Google\\Chrome\\User Data");
             Navigate("https://web.whatsapp.com/");
-
             WaitForLoad();
-
             Thread.Sleep(TimeSpan.FromSeconds(4));
 
-            //WaitForElement(TypeElement.Xpath, "//*[@id='side']/div[1]/div/div/div[2]/div/div[2]");
-
             var elemenSearch = AssignValue(TypeElement.Xpath, "//*[@id='side']/div[1]/div/div/div[2]/div/div[2]", to, 5);
-
             elemenSearch.element.SendKeys(OpenQA.Selenium.Keys.Enter);
 
+            // Insere emojis como texto (ex: ":sorriso:")
             foreach (var emoji in emojis)
             {
                 AssignValue(TypeElement.Xpath, "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p", ":");
@@ -86,19 +77,9 @@ namespace WhatsAppProject
                 element.element.SendKeys(OpenQA.Selenium.Keys.Tab);
             }
 
+            // Envia a mensagem com os emojis
             var elementInput = AssignValue(TypeElement.Xpath, "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p", message);
             elementInput.element.SendKeys(OpenQA.Selenium.Keys.Enter);
-
-        } 
-
-         
-
-     
-
-    
-
-
-
-
+        }
     }
 }
